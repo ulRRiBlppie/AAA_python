53bd605d367169e4dcbb8c22d383194c8646b9e1
Andrey Tarasov <aatarasov@nes.ru>
2022-11-05 13:43:27 +0300

    Second commit, using dicts instead of lists

--- hw5.py
+++ hw5.py
@@ -4,34 +4,36 @@ class CountVectorizer:
     Возвращает лист уникальных слов и кол-во их появлений в листе строк
     """
     def __init__(self):
-        self._unique_words = []
-        self._matrix = []
+        self._dict_of_unique_words = {}
+        self._list_of_dict = []
+        self._list_of_rows = []
+
 
     def fit_transform(self, sample_text: list) -> list:
-
-        for sentence in sample_text:
+        for i, sentence in enumerate(sample_text):
             if not isinstance(sentence, str):
                 raise ValueError(f'На вход ожидается лист строк,\
                                     вы передали{type(sentence)}')
 
             words_in_sentence = [x.lower() for x in sentence.split()]
+            self._list_of_dict.append({})
             for word in words_in_sentence:
-                if word not in self._unique_words:
-                    self._unique_words.append(word)
+                if word not in self._dict_of_unique_words:
+                    self._dict_of_unique_words[word] = 1
+                    self._list_of_dict[i][word] = 1
+                else:
+                    if word not in self._list_of_dict[i].keys():
+                        self._list_of_dict[i][word] = 1
+                    else:
+                        self._list_of_dict[i][word] += 1
 
-        for sentence in sample_text:
-            words_in_sentence = [x.lower() for x in sentence.split()]
-            sentence_count_words = []
-            for unique_word in self._unique_words:
-                sentence_count_words.append(
-                                    words_in_sentence.count(unique_word)
-                                    )
-            self._matrix.append(sentence_count_words)
-
-        return self._matrix
+        
+        self._list_of_rows = [[self._list_of_dict[j].get(word, 0) for word in  self._dict_of_unique_words ] for j in range(i+1)]
+        return self._list_of_rows
+        
 
     def get_feature_names(self) -> list:
-        return self._unique_words
+        return list(self._dict_of_unique_words.keys())
 
 
 if __name__ == '__main__':
@@ -43,3 +45,5 @@ if __name__ == '__main__':
     vectorizer = CountVectorizer()
     count_matrix = vectorizer.fit_transform(corpus)
     print(count_matrix, vectorizer.get_feature_names())
+
+
