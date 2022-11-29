7da7e8a9bce22d48e7aa2e088b7e3decbc004269
Andrey Tarasov <aatarasov@nes.ru>
2022-11-29 23:36:54 +0300

    commit 3

--- hw5.py
+++ hw5.py
@@ -1,42 +1,41 @@
+from collections import Counter
+
 
 class CountVectorizer:
     """
     Возвращает лист уникальных слов и кол-во их появлений в листе строк
     """
     def __init__(self):
-        self._dict_of_unique_words = {}
-        self._list_of_dict = []
-        self._list_of_rows = []
+        self._unique_words = []
+        self._matrix = []
+
+    def _preproc(self, sample_text: list) -> list:
+        """
+        Возвращает лист листов слов, переведенных в нижний регистр,
+        и заполняет self._unique_words
+        """
+        set_of_words = set()
+        processed_text = []
+        for i in range(len(sample_text)):
+            if not isinstance(sample_text[i], str):
+                raise ValueError(f'На вход ожидается лист строк,\
+                                   вы передали{type(sample_text[i])}')
+            sentence = sample_text[i].lower()
+            processed_text.append(sentence.split())
+        set_of_words.update(*processed_text)
+        self._unique_words = sorted(list(set_of_words))
+        return processed_text
 
     def fit_transform(self, sample_text: list) -> list:
-        for i, sentence in enumerate(sample_text):
-            if not isinstance(sentence, str):
-                raise ValueError(f'На вход ожидается лист строк,\
-                                    вы передали{type(sentence)}')
-
-            words_in_sentence = [x.lower() for x in sentence.split()]
-            self._list_of_dict.append({})
-            for word in words_in_sentence:
-                if word not in self._dict_of_unique_words:
-                    self._dict_of_unique_words[word] = 1
-                    self._list_of_dict[i][word] = 1
-                else:
-                    if word not in self._list_of_dict[i].keys():
-                        self._list_of_dict[i][word] = 1
-                    else:
-                        self._list_of_dict[i][word] += 1
-
-        self._list_of_rows = [
-                              [
-                               self._list_of_dict[j].get(word, 0)
-                               for word in self._dict_of_unique_words
-                              ]
-                              for j in range(i+1)
-                             ]
-        return self._list_of_rows
+        processed_text = self._preproc(sample_text)
+        for sentence in processed_text:
+            c = Counter(sentence)
+            sentence_count_words = [c[word] for word in self._unique_words]
+            self._matrix.append(sentence_count_words)
+        return self._matrix
 
     def get_feature_names(self) -> list:
-        return list(self._dict_of_unique_words.keys())
+        return self._unique_words
 
 
 if __name__ == '__main__':
@@ -47,4 +46,4 @@ if __name__ == '__main__':
     ]
     vectorizer = CountVectorizer()
     count_matrix = vectorizer.fit_transform(corpus)
-    print(count_matrix, vectorizer.get_feature_names())
+    print(count_matrix, vectorizer.get_feature_names(), sep='\n')
